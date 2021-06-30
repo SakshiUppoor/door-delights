e3c0bf63da9a54808f62e098d381f25985ed3f07
sakshi uppoor <sakshiuppoor@gmail.com>
2021-06-30 22:59:09 +0530

    Format file using black

--- create_migration_file.py
+++ create_migration_file.py
@@ -15,16 +15,17 @@ from os import listdir
 from pathlib import Path
 import json
 
+
 def take_input():
     print("Details for the migration file: ")
     sql_script = ""
     migration_name = input("Name: ")
     line = input("SQL script: ")
-    sql_script+=line+"\n"
+    sql_script += line + "\n"
 
-    while(line!=""):
+    while line != "":
         line = input()
-        sql_script+=line+"\n"
+        sql_script += line + "\n"
 
     # permissions = []
     # print("Which permissions are needed to run this script?\n (Leave blank if no)\n")
@@ -33,13 +34,13 @@ def take_input():
     # permissions.append("insert") if input("Insert? [-/y] ")!="" else None
     # permissions.append("drop") if input("Drop? [-/y] ")!="" else None
 
-    rollback_script=""
+    rollback_script = ""
     line = input("SQL script for rollback: ")
-    rollback_script+=line+"\n"
+    rollback_script += line + "\n"
 
-    while(line!=""):
+    while line != "":
         line = input()
-        rollback_script+=line+"\n"
+        rollback_script += line + "\n"
 
     # permissions_for_rollback = []
     # print("Which permissions are needed to run this script?\n (Leave blank if no)\n")
@@ -50,34 +51,37 @@ def take_input():
 
     return migration_name, sql_script, rollback_script
 
+
 def get_migration_number():
-    MIGRATION_NO_DIGITS=5
+    MIGRATION_NO_DIGITS = 5
     path = "sql_queries/migrations"
     rollback_path = "sql_queries/rollback"
     try:
         n = len(listdir(path))
-        return "0"*(MIGRATION_NO_DIGITS-len(str(n)))+str(n)
+        return "0" * (MIGRATION_NO_DIGITS - len(str(n))) + str(n)
     except OSError:
         Path(path).mkdir(parents=True, exist_ok=True)
         Path(rollback_path).mkdir(parents=True, exist_ok=True)
-        return "0"*MIGRATION_NO_DIGITS
+        return "0" * MIGRATION_NO_DIGITS
+
 
 def create_migration_files(migration_name, sql_script, rollback_script):
     migration_no = get_migration_number()
-    filename = migration_no+"_"+migration_name.lower().replace(" ","_")+".sql"
-    
+    filename = migration_no + "_" + migration_name.lower().replace(" ", "_") + ".sql"
+
     with open(f"sql_queries/migrations/{filename}", "w") as file:
         file.write(sql_script)
 
     with open(f"sql_queries/rollback/{filename}", "w") as file:
         file.write(rollback_script)
-    
+
     return filename
+
 
 def include_in_releases_json(file_name):
     latest_release = None
     releases = []
-    with open('releases.json') as json_file:
+    with open("releases.json") as json_file:
         releases = json.load(json_file).get("releases")
 
         for i in range(len(releases)):
@@ -85,27 +89,28 @@ def include_in_releases_json(file_name):
                 latest_release = releases[i]["number"]
                 releases[i]["migrations"].append(file_name)
                 break
-    
+
     if not latest_release:
-        if len(releases)==0:
-            latest_release="0.0.0"
+        if len(releases) == 0:
+            latest_release = "0.0.0"
         else:
             last_release = releases[0]["number"]
-            latest_release=input(f"No future released found. (Last release no.: { last_release }) \nPlease enter release number: ")
-        releases.insert(0,(
-            {
-                "number":latest_release,
-                "released": False,
-                "date_released": None,
-                "migrations": [
-                    file_name
-                ]
-            }
-        ))
-    
-    data = {
-        "releases":releases
-    }
+            latest_release = input(
+                f"No future released found. (Last release no.: { last_release }) \nPlease enter release number: "
+            )
+        releases.insert(
+            0,
+            (
+                {
+                    "number": latest_release,
+                    "released": False,
+                    "date_released": None,
+                    "migrations": [file_name],
+                }
+            ),
+        )
+
+    data = {"releases": releases}
 
     with open("releases.json", "w") as file:
         json.dump(data, file, indent=4)
