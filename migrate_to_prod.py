82f77a97a7a08ee4573283da97bc42ac6de9f6d8
sakshi uppoor <sakshiuppoor@gmail.com>
2021-06-30 16:52:16 +0530

    Add migration script

---
+++ migrate_to_prod.py
@@ -0,0 +1,75 @@
+import json
+import psycopg2
+from utils import (
+    parse_args,
+    log_success,
+    log_error,
+    create_server_connection,
+)
+
+# Get release_no and whether the rollback feature is called
+release_no, rollback = parse_args()
+
+# Credentials fetched from environment variables
+connection = create_server_connection()
+cursor = connection.cursor()
+
+"""
+If rollback option is selected, then run sql files from rollback directory
+Else run sql files from migrations directory
+"""
+directory = "rollback" if rollback else "migrations"
+path = f"sql_queries/{directory}"
+
+# Get list of all SQL files to be executed
+release_id = -1
+releases = []
+migrations = []
+
+with open("releases.json") as json_file:
+    releases = json.load(json_file).get("releases")
+    for i in range(len(releases)):
+        if releases[i]["number"] == release_no:
+            release_id = i
+            break
+
+if release_id != -1:
+    migrations = releases[release_id]["migrations"]
+
+else:
+    print("No migrations found.")
+
+failed_flag = 0
+for migration in migrations:
+
+    with open(f"{path}/{migration}", "r") as file:
+        # Fetch SQL queries from the file
+        query = file.read()
+
+        try:
+
+            # TODO Get appropriate permissions
+
+            cursor.execute(query)
+            log_success(release_no, migration)
+
+            # TODO Revoke access permissions
+
+        except (Exception, psycopg2.Error) as error:
+            failed_flag = 1
+            log_error(release_no, migration, error)
+
+            break
+
+    releases[release_id]["released"] = True
+    data = {"releases": releases}
+
+    with open("releases.json", "w") as file:
+        json.dump(data, file, indent=4)
+
+if connection:
+    cursor.close()
+    connection.close()
+
+if migrations and not failed_flag:
+    print("Migrations applied succesfully. ")
---
+++ utils.py
@@ -0,0 +1,153 @@
+import psycopg2
+import os
+import argparse
+from datetime import datetime
+from dotenv import load_dotenv
+
+load_dotenv()
+
+"""
+For parsing arguments provided from the command line 
+and displaying the help menu
+
+Arguments:
+-
+
+Returns:
+    - release_no: str 
+        Release no. provided from the command line. This is used for 
+        determining which migrations have to be applied
+    - rollback: bool
+        indicates whether rollback option is selected or not
+"""
+
+
+def parse_args():
+    parser = argparse.ArgumentParser(
+        description="Migrate DB changes between environments."
+    )
+
+    parser.add_argument(
+        "release_no",
+        help="Release number of the migrations you want to apply",
+        type=str,
+    )
+
+    parser.add_argument(
+        "--rollback",
+        action="store_true",
+        help="Rollback migrations applied in this release",
+    )
+
+    # Return release number and optional rollback option from argument
+    args = parser.parse_args()
+    return args.release_no, args.rollback
+
+
+"""
+For maintaining log of applied/failed migrations and errors
+Append message along with timestamp to log files
+
+Arguments: 
+    - filename: str 
+        Name of log file in which the message is to be appended.
+        for eg: "db.log", "error.log"
+    - message: str 
+        Message to be appended in the log file
+        for eg: "Migration applied successfully"
+
+Returns:
+-
+"""
+
+
+def write_to_log_file(filename, message):
+    now = datetime.now()
+    with open(filename, "a") as log_file:
+        log_file.write(f"[{now}] {message} \n")
+
+
+"""
+Once a migration is applied successfully, it's appended 
+in the message and db log files
+
+Arguments: 
+    - release_no: str 
+        Release number
+    - migration_file_name: str 
+        Name of the SQL file which was migrated
+
+Returns:
+-
+"""
+
+
+def log_success(release_no, migration_file_name):
+    write_to_log_file(
+        "db.log",
+        f"Release-{release_no} Succesfully applied migration {migration_file_name}",
+    )
+    write_to_log_file(
+        "message.log",
+        f"Release-{release_no} Succesfully applied migration {migration_file_name}",
+    )
+
+"""
+If a migration fails, the error is logged in 
+messages and error log files
+
+Arguments: 
+    - release_no: str 
+        Release number
+    - migration_file_name: str 
+        Name of the SQL file which was migrated
+    - error: str 
+        Desciption of the error that occured
+
+Returns:
+-
+"""
+def log_error(release_no, migration_file_name, error):
+    print(
+        f"Release-{release_no} Failed to run apply migration {migration_file_name}. Check error.log for details."
+    )
+
+    write_to_log_file(
+        "message.log",
+        f"Release-{release_no} Failed to run apply migration {migration_file_name}. Check error.log for details.",
+    )
+    write_to_log_file(
+        "error.log",
+        f"Error while applying migration: {error}".replace("\n", ""),
+    )
+
+
+"""
+For creating a connection with the database server
+Arguments: 
+-
+
+Returns: 
+    - connection
+"""
+
+
+def create_server_connection():
+    connection = None
+    try:
+        connection = psycopg2.connect(
+            host=os.getenv("HOST_NAME"),
+            database=os.getenv("DATABASE_NAME"),
+            user=os.getenv("USER_NAME"),
+            password=os.getenv("USER_PASSWORD"),
+        )
+
+    except (Exception, psycopg2.DatabaseError) as error:
+        write_to_log_file(
+            "db.log", "Error while connecting to database. Check error.log for details"
+        )
+        write_to_log_file("error.log", f"Error while connecting to database: {error}")
+        print(f"Error while connecting to database. Check error.log for details \n")
+        exit()
+
+    return connection
